0f6496b69275fd70244cbd64be57d7e868cfdcd4
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-05-27 11:45:05 +0600

    Update fp.py

--- fp.py
+++ fp.py
@@ -1,1 +1,12 @@
-from tkinter import Frame, Canvas, CENTER, ROUND
+import tkinter as tk
+from tkinter import tkk
+from editBar import EditBar
+from imageViewer import ImageViewer
+
+class Fp(tk.Tk):
+  
+  def__init__(self):
+    tk.Tk.__init__(self)
+    
+    self.filename = ""
+    self.original_image = None
