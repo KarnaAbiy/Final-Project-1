f6b8282000d8c00d9f332bfd3299bc15f275182a
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-05-29 18:19:16 +0600

    Update fp.py

--- fpcode/fp.py
+++ fpcode/fp.py
@@ -1,31 +1,31 @@
 from tkinter import *
+
 class PhotoEd:
-  def __init__(self):
-    self.root = Tk()
-    self.init()
+
+	def __init__(self):
+        self.root = Tk()
+        self.init()
     
-  def init(self):
-    self.root.title("Photo Editor")
-    self.root.iconbitmap()
+    def init(self):
+        self.root.title("Photo Editor")
+        self.root.iconphoto(True, PhotoImage("D:\Images\Ala-Too_Icon")
     
-    self.root.bind("<Escape>", self._close)
+        self.root.bind("<Escape>", self._close)
   
-  def run(self):
-    self.draw_menu()
-    self.draw_widgets()
+    def run(self):
+        self.draw_menu()
+        self.draw_widgets()
     
-    self.root.mainloop()
+        self.root.mainloop()
     
     def draw_menu(self):
-      pass
+        pass
     
     def draw_widgets(self):
-      pass
+        pass
    
     def close(self):
-      self.root.quit()
+        self.root.quit()
       
 if __name__  == "__main__":
-  PhotoEd().run
-      
-      
+    PhotoEd().run
