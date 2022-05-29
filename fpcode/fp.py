a1265243b66137ab26147bb542106c2fce230dfd
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-05-29 17:59:36 +0600

    Update fp.py

--- fpcode/fp.py
+++ fpcode/fp.py
@@ -1,4 +1,31 @@
-ımport tkınter as tk
-from tkınter ımport ttk
-from edıtBar ımport EdıtBar
-from ımageVıewer ımport ImageVıewer
+from tkinter import *
+class PhotoEd:
+  def __init__(self):
+    self.root = Tk()
+    self.init()
+    
+  def init(self):
+    self.root.title("Photo Editor")
+    self.root.iconbitmap()
+    
+    self.root.bind("<Escape>", self._close)
+  
+  def run(self):
+    self.draw_menu()
+    self.draw_widgets()
+    
+    self.root.mainloop()
+    
+    def draw_menu(self):
+      pass
+    
+    def draw_widgets(self):
+      pass
+   
+    def close(self):
+      self.root.quit()
+      
+if __name__  == "__main__":
+  PhotoEd().run
+      
+      
