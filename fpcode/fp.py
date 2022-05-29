c6eb3609f38c8fdea6b105c81cda249d0afd6552
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-05-30 01:55:19 +0600

    Update fp.py

--- fpcode/fp.py
+++ fpcode/fp.py
@@ -1,31 +1,8 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from PIL import Image, ImageTk, ImageFilter, ImageEnhance, ImageOps
+import os
 
-class PhotoEd:
-
-	def __init__(self):
-        self.root = Tk()
-        self.init()
-    
-    def init(self):
-        self.root.title("Photo Editor")
-        self.root.iconphoto(True, PhotoImage("D:\Images\Ala-Too_Icon")
-    
-        self.root.bind("<Escape>", self._close)
-  
-    def run(self):
-        self.draw_menu()
-        self.draw_widgets()
-    
-        self.root.mainloop()
-    
-    def draw_menu(self):
-        pass
-    
-    def draw_widgets(self):
-        pass
-   
-    def close(self):
-        self.root.quit()
-      
-if __name__  == "__main__":
-    PhotoEd().run
+root = Tk()
